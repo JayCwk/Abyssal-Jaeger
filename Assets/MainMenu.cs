b0b1419df0de7446eeb57f345161ecd6d302843f
CwY020212 <waiyau020212@gmail.com>
2024-04-03 20:56:52 +0800

    Done Main Menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -9,6 +9,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadSceneAsync(1);
     }
 
+    public void PlayGame1()
+    {
+        SceneManager.LoadSceneAsync(2);
+    }
+
     public void QuitGame()
     {
         Application.Quit();
