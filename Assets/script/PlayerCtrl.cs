eadf94bed40affbf1d7fe59ff2f049485c698668
JayCwk <cheahwk@1utar.my>
2024-04-17 19:25:53 +0800

    Added ship switching

--- Assets/script/PlayerCtrl.cs
+++ Assets/script/PlayerCtrl.cs
@@ -14,11 +14,16 @@ public class PlayerCtrl : MonoBehaviour
     public float shootInterval = 0.3f; // Adjust this to change the time between shots
     public float shootForce = 10f;
 
-
+    public SpriteRenderer shipSprite;
+    public Sprite ship1;
+    public Sprite ship2;
+    private Sprite activeShip;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        shipSprite = gameObject.GetComponent<SpriteRenderer>();
+        activeShip = ship1;
 
         // Start shooting automatically when the object is enabled
         InvokeRepeating("Shoot", 0f, shootInterval);
@@ -27,8 +32,30 @@ public class PlayerCtrl : MonoBehaviour
     
     void Update()
     {
+        //player tilting movement
         dirX = Input.acceleration.x * moveSpeed;
+        //set movement area
         transform.position = new Vector2(Mathf.Clamp(transform.position.x, -5.8f, 5.8f), transform.position.y);
+
+        for (int i = 0; i < Input.touchCount; i++) 
+        {
+            if (Input.GetTouch(i).phase == TouchPhase.Began)
+            {
+                Debug.Log("touch");
+                if(activeShip == ship1)
+                {
+                    activeShip = ship2;
+                    shipSprite.sprite = ship2;
+                }
+                else if (activeShip == ship2)
+                {
+                    activeShip = ship1;
+                    shipSprite.sprite = ship1;
+                }
+
+            }
+        }
+
     }
 
     private void FixedUpdate()
@@ -47,5 +74,10 @@ public class PlayerCtrl : MonoBehaviour
             rb.velocity = shootPoint.up * shootForce; // Shoot in the direction the shootPoint is facing
         }
         // Add any additional logic or effects here
+    }
+
+    void ChangeSprite(Sprite sprite)
+    {
+        shipSprite.sprite = sprite;
     }
 }
