a7ca0de963592b44cc886e9f527a2c1302c5c75a
CwY020212 <waiyau020212@gmail.com>
2024-05-08 10:38:43 +0800

    volume done

--- Assets/script/VolumeSettings.cs
+++ Assets/script/VolumeSettings.cs
@@ -10,15 +10,19 @@ public class VolumeSettings : MonoBehaviour
     [SerializeField] private Slider mySlider1;
     [SerializeField] private Slider mySlider2;
 
+    public static VolumeSettings instance;
+
     public void SetMusicVolume()
     {
         float volume = mySlider1.value;
         myAudio.SetFloat("Music", Mathf.Log10(volume)*20);
+        PlayerPrefs.Save();
     }
 
     public void SetMusicVolume1()
     {
         float volume = mySlider2.value;
         myAudio.SetFloat("SFX", Mathf.Log10(volume) * 20);
+        PlayerPrefs.Save();
     }
 }
